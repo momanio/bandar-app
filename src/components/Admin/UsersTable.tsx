788f1275596729bad9b443e49f72ef7998061556
momanio <o.a.momani@gmail.com>
2024-04-29 23:52:37 +0300

    Test

--- src/components/Admin/UsersTable.tsx
+++ src/components/Admin/UsersTable.tsx
@@ -82,6 +82,7 @@ const UsersTable = () => {
         <RequestCard age="18" email="mustaf@ggg" name="mustafa" />
         <RequestCard age="18" email="mustaf@ggg" name="mustafa" />
         <RequestCard age="18" email="mustaf@ggg" name="mustafa" />
+        <RequestCard age="18" email="mustaf@ggg" name="mustafa" />
       </div>
     </>
   );
