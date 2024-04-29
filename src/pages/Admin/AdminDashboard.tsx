918e77ae1a4285cc3aafba071c7cf553fcfa5f97
momanio <o.a.momani@gmail.com>
2024-04-29 21:07:46 +0300

    UI for UsersTable, new Title component and some changes on services

--- src/pages/Admin/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard.tsx
@@ -1,13 +1,11 @@
 import MainLayout from "UI/Admin/MainLayout";
 import Header from "components/Admin/Header";
 import UsersTable from "components/Admin/UsersTable";
-import Title from "components/shared/Title";
 
 const AdminDashboard = () => {
   return (
     <MainLayout>
       <Header />
-      <Title>قائمة طلبات التسجيل</Title>
       <UsersTable />
     </MainLayout>
   );
