8f5b69c07cbb1e14f109f03c2cf2085f1e8aeaa6
Mustafa <mustafaalowisi@gmail.com>
2024-05-06 12:08:43 +0300

    fix issues

--- src/components/Admin/Dashboard/RequestCard.tsx
+++ src/components/Admin/Dashboard/RequestCard.tsx
@@ -3,10 +3,10 @@ import CreateSubscDialog from "../../shared/CreateSubscDialog";
 import ShowUserDialog from "./ShowUserDialog";
 import { Button } from "@mui/material";
 import { useState } from "react";
-import { ReactSVG } from "react-svg";
 import addIcon from "assets/icons/addIcon.svg";
 import CreateSubsStepTow from "components/shared/CreateSubsStepTow";
-
+import { ReactSVG } from "react-svg";
+import eyeIcon from "assets/icons/eye.svg";
 const RequestCard = ({
   name,
   mobile,
@@ -22,6 +22,7 @@ const RequestCard = ({
 }) => {
   const [open, setOpen] = useState(false);
   const [openTow, setOpenTow] = useState(false);
+  const [settingModel, setSettingModel] = useState(false);
 
   return (
     <div className="flex items-center w-full py-4 pr-[3.125rem] pl-[2.813rem] justify-between odd:bg-[#1a1d1f] even:bg-[#272b304d] text-[#f3f3f4] ">
@@ -31,22 +32,6 @@ const RequestCard = ({
       <div className="w-fit ">{age}</div>
       <div className="w-fit ">{date}</div>
       <div className="flex gap-4 w-fit">
-        <CreateSubscDialog
-          handleClose={() => {
-            setOpen(!open);
-          }}
-          open={open}
-          openStepTow={() => {
-            setOpen(!open);
-            setOpenTow(!openTow);
-          }}
-        />
-        <CreateSubsStepTow
-          handleClose={() => {
-            setOpenTow(!openTow);
-          }}
-          open={openTow}
-        />
         <Button
           onClick={() => {
             setOpen(!open);
@@ -71,7 +56,50 @@ const RequestCard = ({
           <ReactSVG src={addIcon} />
           انشاء اشتراك
         </Button>
-        <ShowUserDialog />
+        <Button
+          onClick={() => {
+            setSettingModel(!settingModel);
+          }}
+          sx={{
+            bgcolor: "#333638",
+            color: "#fff",
+            borderColor: "#333638",
+            display: "flex",
+            padding: "0.375rem",
+            minWidth: "2rem",
+            borderRadius: "0.75rem",
+            alignItems: "center",
+            ":hover": {
+              borderColor: "#333638",
+              bgcolor: "#333638",
+            },
+          }}
+        >
+          <ReactSVG src={eyeIcon} />
+          عرض
+        </Button>
+        <CreateSubscDialog
+          handleClose={() => {
+            setOpen(!open);
+          }}
+          open={open}
+          openStepTow={() => {
+            setOpen(!open);
+            setOpenTow(!openTow);
+          }}
+        />
+        <CreateSubsStepTow
+          handleClose={() => {
+            setOpenTow(!openTow);
+          }}
+          open={openTow}
+        />
+        <ShowUserDialog
+          handleClose={() => {
+            setSettingModel(!settingModel);
+          }}
+          open={settingModel}
+        />
         <DeleteDialog
           title="حذف طلب التسجيل"
           subTitle="عند حذفك لهذا الطلب لن تتمكن من استرجاعه"
--- src/components/Admin/Dashboard/ShowUserDialog.tsx
+++ src/components/Admin/Dashboard/ShowUserDialog.tsx
@@ -1,72 +1,45 @@
 import CloseIcon from "@mui/icons-material/Close";
 import { Button, Dialog, IconButton } from "@mui/material";
-import eyeIcon from "assets/icons/eye.svg";
+
 import UserSetting from "components/shared/UserSetting";
 import { useState } from "react";
-import { ReactSVG } from "react-svg";
 
-const ShowUserDialog = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+const ShowUserDialog = ({
+  open,
+  handleClose,
+}: {
+  open: boolean;
+  handleClose: () => void;
+}) => {
   return (
-    <div>
-      <Button
-        onClick={handleClickOpen}
-        sx={{
-          bgcolor: "#333638",
-          color: "#fff",
-          borderColor: "#333638",
-          display: "flex",
-          padding: "0.375rem",
-          minWidth: "2rem",
-          borderRadius: "0.75rem",
+    <Dialog
+      PaperProps={{
+        style: {
+          backgroundColor: "unset",
+          margin: "0",
+          width: "88.65rem",
+          height: "100%",
+          maxWidth: "88.65rem",
           alignItems: "center",
-          ":hover": {
-            borderColor: "#333638",
-            bgcolor: "#333638",
-          },
+        },
+      }}
+      onClose={handleClose}
+      open={open}
+    >
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{
+          position: "absolute",
+          left: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
         }}
       >
-        <ReactSVG src={eyeIcon} />
-        عرض
-      </Button>
-      <Dialog
-        PaperProps={{
-          style: {
-            backgroundColor: "unset",
-            margin: "0",
-            width: "100%",
-          },
-        }}
-        onClose={handleClose}
-        open={open}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={() => {
-            handleClose();
-          }}
-          sx={{
-            position: "absolute",
-            left: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <div className="w-full">
-          <UserSetting />
-        </div>
-      </Dialog>
-    </div>
+        <CloseIcon />
+      </IconButton>
+      <UserSetting />
+    </Dialog>
   );
 };
 export default ShowUserDialog;
